978f78a44f1922309fd11570bf190facdffe959e
Gianni Perez <gianniperez@gmail.com>
2021-10-17 12:36:59 -0400

    2021 revision

--- shannon-entropy.py
+++ shannon-entropy.py
@@ -4,10 +4,11 @@
 #                  string of symbols, based on the frequency of the symbols.
 # author          :Gianni Perez
 # date            :20170901
-# version         :0.2
+# version         :0.3
+# revised         :20211017
 # usage           :python shannon_entropy.py
 # notes           :
-# python_version  :3.6
+# python_version  :3.7+
 # ============================================================================
 
 import math
@@ -20,7 +21,7 @@ def shannon_entropy(data):
     for character in data:
         stack[character] = round(data.count(character) / len(data), 5)
         symbol_list[character] = data.count(character)
-    print('\nSymbol-occurrence frequencies:\n')
+    print("\nSymbol-occurrence frequencies:\n")
     for symbol in stack:
         print("{0} --> {1} -- {2}".format(symbol, stack[symbol], symbol_list[symbol]))
     return symbol_frequency(stack)
@@ -32,9 +33,9 @@ def symbol_frequency(symbol_set):
     return entropy
 
 
-if __name__ == '__main__':
-    m = input('\nEnter the message: ')
+if __name__ == "__main__":
+    m = input("\nEnter the message: ")
     bits = shannon_entropy(m)
-    print('\nH(X) = {0} bits. Rounded to {1} bits/symbol, '.format(bits, round(bits)))
-    print('it will take {0} bits to optimally encode "{1}"'.format(len(m) * round(bits), m))
-    print('\nMetric entropy: %.5f' % (bits / len(m)))
+    print("\nH(X) = {0} bits. Rounded to {1} bits/symbol, ".format(bits, round(bits)))
+    print("it will take {0} bits to optimally encode '{1}''".format(len(m) * round(bits), m))
+    print("\nMetric entropy: %.5f" % (bits / len(m)))
